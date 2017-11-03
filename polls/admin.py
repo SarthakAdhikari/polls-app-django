b7c949f33dee821bb90d636cd4c41f47b697a6f4
Sarthak Adhikari <adhikari.sarthak@gmail.com>
2017-11-03 10:22:04 +0545

    Completed polls app, no front end)

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Question,Choice
+from .models import Choice,Question
 # Register your models here.
-admin.site.register(Question)
-#admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra=3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date','question_text']
+    list_filter = ['pub_date']
+    fieldsets=[
+        (None,{'fields':['question_text']}),
+        ('Date Information', {'fields':['pub_date']}),
+        ]
+    inlines = [ChoiceInline]
+admin.site.register(Question,QuestionAdmin)
